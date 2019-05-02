d662bc4cd2bdeb4e01f197c96d8937ba29c4096f
Nils Wilmet <n.wilmet@agiirnetwork.com>
2019-05-02 10:15:21 +0200

    [FEAT] EventList V2

--- components/Event/EventList.js
+++ components/Event/EventList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View, Text, FlatList} from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 //Components
 import EventItem from './EventItem'
 
@@ -11,35 +11,50 @@ class EventList extends React.Component {
             events: [{
                 id: 1,
                 title: "Mon Event 1",
-                description: "Bonjour bienvenue dans mon superbe event",
+                description: "Bonjour bienvenue dans mon superbe event qui va jusqu'au bout de la niiiiiight",
                 image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABABAMAAABYR2ztAAAAIVBMVEXMzMyWlpbFxcWjo6O+vr63t7ecnJyqqqqbm5uxsbGampoKZyAaAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAU0lEQVRIiWNgGAWjYBQMd8Bk7KCITGMAZqeAQgZ2zwAwjQ2wmYU6ArVDaGyA0aWTkYHBFEpjM0FpoaABUzGExqaAJdmBkYFdNQBMj4JRMAoGOwAAPNIL2qWeApgAAAAASUVORK5CYII=",
-                inviteCode: "G7F87DE"
+                inviteCode: "G7F87DE",
+                guests: 78,
+                status: "OPEN",
             },
             {
                 id: 2,
                 title: "Mon Event 2",
                 description: "Bonjour bienvenue dans mon autre superbe event",
                 image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABABAMAAABYR2ztAAAAIVBMVEXMzMyWlpbFxcWjo6O+vr63t7ecnJyqqqqbm5uxsbGampoKZyAaAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAU0lEQVRIiWNgGAWjYBQMd8Bk7KCITGMAZqeAQgZ2zwAwjQ2wmYU6ArVDaGyA0aWTkYHBFEpjM0FpoaABUzGExqaAJdmBkYFdNQBMj4JRMAoGOwAAPNIL2qWeApgAAAAASUVORK5CYII=",
-                inviteCode: "7DFAB5C"
+                inviteCode: "7DFAB5C",
+                guests: 42,
+                status: "PREPARING"
             },
             {
                 id: 3,
                 title: "Mon Event 3",
-                description: "Bonjour bienvenue dans mon dernier event",
+                description: "Coucou ! et bienvenue dans mon troisième event",
                 image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABABAMAAABYR2ztAAAAIVBMVEXMzMyWlpbFxcWjo6O+vr63t7ecnJyqqqqbm5uxsbGampoKZyAaAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAU0lEQVRIiWNgGAWjYBQMd8Bk7KCITGMAZqeAQgZ2zwAwjQ2wmYU6ArVDaGyA0aWTkYHBFEpjM0FpoaABUzGExqaAJdmBkYFdNQBMj4JRMAoGOwAAPNIL2qWeApgAAAAASUVORK5CYII=",
-                inviteCode: "875E9D1"
+                inviteCode: "875E9D1",
+                guests: 102,
+                status: "ENDED"
+            },
+            {
+                id: 3,
+                title: "Mon Event 4",
+                description: "Bonjour bienvenue dans mon tout dernier event de la liste !",
+                image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABABAMAAABYR2ztAAAAIVBMVEXMzMyWlpbFxcWjo6O+vr63t7ecnJyqqqqbm5uxsbGampoKZyAaAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAU0lEQVRIiWNgGAWjYBQMd8Bk7KCITGMAZqeAQgZ2zwAwjQ2wmYU6ArVDaGyA0aWTkYHBFEpjM0FpoaABUzGExqaAJdmBkYFdNQBMj4JRMAoGOwAAPNIL2qWeApgAAAAASUVORK5CYII=",
+                inviteCode: "78EFB12",
+                guests: 425,
+                status: "UNAVAILABLE"
             }]
         }
     }
 
     render() {
-        return(
+        return (
             <View>
                 <FlatList
                     data={this.state.events}
-                    renderItem={({item, index}) => {
+                    renderItem={({ item, index }) => {
                         return (
-                            <EventItem key={item.id} event={item} type={index % 2 == 1 ? "default" : "reversed"} />
+                            <EventItem key={item.id} event={item} type="default" />
                         )
                     }}
                 />
--- components/Event/EventList.style.js
+++ components/Event/EventList.style.js
@@ -1,4 +1,5 @@
 import { StyleSheet, Dimensions } from "react-native";
+import Colors from "../../consts/Colors";
 
 export default StyleSheet.create({
     container: {
@@ -6,38 +7,60 @@ export default StyleSheet.create({
         display: "flex",
         flexWrap: "wrap",
         flexDirection: "row",
-        flex: 10
+        flex: 10,
     },
-    containerRev: {
-        margin: 8,
-        display: "flex",
-        flexWrap: "wrap",
-        flexDirection: "row-reverse",
-        flex: 10
+    imageContainer: {
+        flex: 3,
+        marginRight: 12,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        borderRadius: 5,
+        overflow: "visible"
     },
     image: {
-        width: 64,
-        height: 64,
-        marginRight: 12,
-        flex: 2
-    },
-    imageRev: {
-        width: 64,
-        height: 64,
-        marginLeft: 12
+        width: 96,
+        height: 96,
+        borderRadius: 5
     },
     containerText: {
         display: "flex",
         flexDirection: "column",
-        flex: 8
+        flex: 7
     },
-    titleCode: {
+    titleContainer: {
+        display: "flex",
+        flexDirection: "row"
+    },
+    badgeGuestsContainer: {
         display: "flex",
         flexDirection: "row",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        width: 200,
+        flex: 10
+    },
+    badgeCode: {
+        flex: 7
+    },
+    guestCount: {
+        marginLeft: 18,
+        fontSize: 13
+    },
+    guestIcon: {
+        marginLeft: 3
     },
     title: {
         fontSize: 14,
         marginRight: 5,
-        fontWeight: "bold"
+        fontWeight: "bold",
+    },
+    description: {
+        fontSize: 14
     }
 });

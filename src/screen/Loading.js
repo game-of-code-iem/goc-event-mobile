92c29b214e7d3b5f98b083fe0bbed5a3f77ada13
ThomasP <trolldu25@live.fr>
2019-05-02 14:37:32 +0200

    [FEAT] ReduxAction

--- src/screen/Loading.js
+++ src/screen/Loading.js
@@ -1,37 +1,36 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 import { connect } from 'react-redux';
 
-
 const mapStateToProps = (state) => ({
-	active: state.visibilityFilter
+	response: state.response
 });
 
 const mapDispatchToProps = (dispatch) => ({
 	setVisibilityFilter: (body) => dispatch(setVisibilityFilter(body))
 });
 
-
 class Loading extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { status: '400' };
+	}
 
-    constructor(props){
-        super(props)
-        this.state = {status:"400"}
-    }
+	componentDidUpdate(prevProps, prevState) {
+		console.log(this.props.response);
 
-    componentDidUpdate(prevProps, prevState) {
-        if(this.props.response.status == "200") {
-            this.props.navigation.navigate('Login')
-        } 
-    }
-    
-    render() {
-        return (
-            <View>
-                <Text> Loading ...</Text>
-            </View>
-        )
-    }
+		if (this.props.response.status == '200') {
+			this.props.navigation.navigate('Login');
+		}
+	}
+
+	render() {
+		return (
+			<View>
+				<Text> Loading ...</Text>
+			</View>
+		);
+	}
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Loading);
